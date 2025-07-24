2c7bba217de7e4159090dcf042a2e81ab48f2052
rishikeshsreehari <rishikeshshari@gmail.com>
2025-07-25 03:26:49 +0400

    Update dotfiles

--- macos-setup.sh
+++ macos-setup.sh
@@ -5,9 +5,13 @@ echo "Setting up macOS preferences..."
 # Show hidden files in Finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Other useful macOS defaults
+# Dock optimizations
 defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0.3
+defaults write com.apple.dock autohide-time-modifier -float 0.25
 defaults write com.apple.dock tilesize -int 36
+
+# Other useful macOS defaults
 defaults write com.apple.NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
@@ -17,3 +21,6 @@ killall Finder
 killall Dock
 
 echo "macOS preferences updated!"
+echo "- Hidden files now visible in Finder"
+echo "- Dock set to auto-hide with faster animations"
+echo "- Smaller dock size for more screen space"
